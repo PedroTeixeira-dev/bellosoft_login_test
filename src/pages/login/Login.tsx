8186c02136e7e97a9d06eb6ed5ec84f6bed38bed
Pedro Teixeira <pedro15at@gmail.com>
2024-06-28 12:29:13 -0300

    feat: creating the inputs fields

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,19 @@
 import BackButton from "../../components/backbutton/backbutton"
 import { ContainerLogin } from "../../components/container/styled"
-import { ContainerForm, ContainerText, ContainerTop } from "./styled"
+import { ContainerForm, ContainerText, ContainerTop, InputField, InputS, MainButton } from "./styled"
 import leaf from "../../images/leaf.png"
+import * as I from 'react-bootstrap-icons'
+import { useState } from "react"
 
 const Login = () => {
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+          setPasswordVisible(!passwordVisible);
+        };
+
+
     return (
         <ContainerLogin>
             <ContainerTop>
@@ -16,13 +26,24 @@ const Login = () => {
                 <h2>Please login to continue</h2>
             </ContainerText>
             <ContainerForm>
-                <input type="text" placeholder="Email Address" />
-                <input type="password" placeholder="Password" />
-                <button>Login</button>
+                <InputField>
+                <label>Email Address</label>
+                <I.EnvelopeFill color="#8B8B8B" size={24} className="icon"/>
+                <InputS type="text" placeholder="Your email address" />
+                </InputField>
+                <InputField>
+                <label>Password</label>
+                <I.LockFill color="#8B8B8B" size={24} className="icon"/>
+                <I.EyeFill color="#8B8B8B" size={24} onClick={togglePasswordVisibility} className="icon2"/>
+                <InputS type={passwordVisible ? "text" : "password"} placeholder="Enter your password" />
+                </InputField>
+                <MainButton>Login</MainButton>
                 <p>Forgot Password</p>
             </ContainerForm>
         </ContainerLogin>
     )
 }
 
+
 export default Login
+
--- src/pages/login/styled.ts
+++ src/pages/login/styled.ts
@@ -37,5 +37,77 @@ color: #8B8B8B;
 `
 
 export const ContainerForm = styled.form`
+display: flex;
+flex-direction: column;
+margin-top: 32px;
+margin-left: 32px;
+margin-right: 32px;
 
+
+p {
+    text-align: center;
+    font-size: 13px;
+    line-height: 15.21px;
+    text-align: center;
+    color: #BC8363;
+    cursor: pointer;
+
+}
+`
+export const InputS = styled.input`
+border: 1px solid #E9ECEF;
+border-radius: 8px;
+height: 64px;
+margin-bottom: 18px;
+width: 100%;
+padding-left: 62px;
+
+&&::placeholder {
+    color:#8B8B8B ;
+    font-size: 13px;
+    line-height: 15px;
+}
+`
+
+export const MainButton = styled.button`
+    text-decoration: none;
+    display: inline-block;
+    background-color: #BC8363;
+    color: rgba(255, 255, 255, 1);
+    font-size: 13px;
+    width: 100%;
+    height: 55px;
+    padding: 20px;
+    gap: 18px;
+    border-radius: 8px;
+    opacity: 0px;
+    border: none;
+    margin-top: 2px;
+    margin-bottom: 20px;
+    cursor: pointer;
+`
+
+export const InputField = styled.div`
+width: 100%;
+position: relative;
+
+.icon {
+position: absolute;
+top: 22px;
+left: 20px;
+}
+
+.icon2 {
+position: absolute;
+top: 22px;
+right: 20px;
+}
+
+label {
+    position: absolute;
+    top: 8px;
+    left: 62px;
+    color: #20130B;
+    font-size: 13px;
+}
 `
